3cc32fa665a5d728e93f58441a9e9f8816490dfb
TortelloSte <stefano.perdicchia1999@gmail.com>
2022-12-12 21:02:29 +0100

    random forest a posto

--- utilities/model.py
+++ utilities/model.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error
+# from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
 
 def create_model(df):
