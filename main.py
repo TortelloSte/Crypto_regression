d9d7027a5a6651352f9ecd50fc6b2df6400ff78d
Stefano Perdicchia <s.perdicchia@itsrizzoli.it>
2022-12-12 14:41:05 +0100

    aggiunta grafici

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from utilities.file_operation import read_files, write_errors_file
+from utilities.file_operation import read_files
+from utilities.general_function import get_error_data
 
 if __name__ == '__main__':
     filepath = 'C:/Users/StefanoPerdicchia/Crypto_regression/archive'
     output_error_path = "C:/Users/StefanoPerdicchia/Crypto_regression/error.json"
     test = read_files(filepath, output_error_path)
-
-    # plt.plot(p, color = 'r')
-    # plt.plot(X_test, alpha = .2)
-    # plt.show()
+    print(get_error_data(output_error_path))
--- utilities/model.py
+++ utilities/model.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
@@ -9,13 +10,11 @@ def create_model(df):
     X = np.array([df.open, df.low, df.high])
     X = X.transpose()
     y = df.close
-    # print(X.shape)
-    # print(y.shape)
     X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.3, shuffle=None)
     model = LinearRegression()
     model.fit(X_train, y_train)
     p = model.predict(X_test)
+    plt.plot(p, color = 'r')
+    plt.plot(X_test, alpha = .2)
+    plt.show()
     return mean_absolute_error(y_test,p)
-
-
-
